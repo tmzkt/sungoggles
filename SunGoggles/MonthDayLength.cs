08b986f093e30d73ee225398b113c8233a438830
Scott Kiedeisch <scott.kiedeisch@ascensus.com>
2019-01-15 17:18:20 -0500

    add first code files

---
+++ SunGoggles/MonthDayLength.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeAndDateScraper
+{
+    [Serializable]
+    class MonthDayLength
+    {
+        public int Month { get; set; }
+        public int Year { get; set;  }
+        public string Location { get; set; }
+        public Dictionary<int, string> DayOfMonthToDayLength { get; set; }
+    }
+}
---
+++ SunGoggles/Program.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace TimeAndDateScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.timeanddate.com/sun/usa/philadelphia?month=1&year=2019");
+
+            var s = doc.DocumentNode.SelectNodes("//tr[@data-day]");
+
+            MonthDayLength myobj = new MonthDayLength();
+            myobj.Month = 1;
+            myobj.Year = 2019;
+            myobj.Location = "philadelphia";
+            myobj.DayOfMonthToDayLength = new System.Collections.Generic.Dictionary<int, string>();
+            foreach (HtmlNode node in s)
+            {
+                int dayOfMonth = Convert.ToInt32(node.Attributes["data-day"].Value);
+                string dayLength = node.ChildNodes[3].InnerText;
+                string difference = node.ChildNodes[4].InnerText;
+                Console.WriteLine($"Day {dayLength}, difference {difference}");
+                myobj.DayOfMonthToDayLength.Add(dayOfMonth, dayLength);
+            }
+
+            Stream stream = null;
+            try
+            {
+                stream = new FileStream($"cache{myobj.Year}-{myobj.Month}", FileMode.Create);
+                new BinaryFormatter().Serialize(stream, myobj);
+            }
+            finally
+            {
+                stream?.Close();
+            }
+        }
+    }
+}
