8bc513bb28913735d9b190ed8c4ae64a87313418
Scott <23584906+tmzkt@users.noreply.github.com>
2019-01-28 17:34:51 -0500

    add basic data access class for timeanddate.com

---
+++ SunGoggles/TimeAndDateDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunGoggles
+{
+    class TimeAndDateDataAccess
+    {
+        public string Country { get; }
+        public string City { get; }
+        public int Month { get; }
+        public int Year { get; }
+
+        public TimeAndDateDataAccess(string country, string city, int month, int year)
+        {
+            this.Country = country;
+            this.City = city;
+            this.Month = month;
+            this.Year = year;
+        }
+    }
+}
